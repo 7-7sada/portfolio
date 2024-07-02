a96bdecf2521c372bd6aaa5131bb689a9b79c51d
7-7sada <jedsada.chot@gmail.com>
2024-07-02 14:21:34 +0700

    dark mode selector button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="container mx-auto max-w-2xl">
         <header class="flex justify-between items-center mt-5">
-            <div>
+            <div class="flex items-center space-x-12">
+              <div>
                 <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Jair9und</NuxtLink>
+              </div>
+
+              <Menu />
             </div>
 
-            <Menu />
+            <ClientOnly>
+              <ColorModeSelector />
+            </ClientOnly>
         </header>
 
         <main class="p-2 mt-10">
@@ -15,9 +21,6 @@
 </template>
 
 <script setup lang="ts">
-const colorMode = useColorMode()
-console.log(colorMode.preference)
-colorMode.preference = 'light'
 useHead({
   titleTemplate: '%s - Jairgund',
   link: [
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ export default defineNuxtConfig({
   modules: [
     "@nuxtjs/tailwindcss", 
     "@nuxtjs/color-mode"
-  ]
+  ],
 })
