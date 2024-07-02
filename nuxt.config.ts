290b25dad7428a34a99358d7275b3eb2b098dd3a
7-7sada <jedsada.chot@gmail.com>
2024-07-02 17:05:37 +0700

    images and code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+    "@nuxt/image"
+  ],
+  content:{
+    highlight: {
+      theme:{
+        default: 'min-light',
+        dark: 'min-dark'
+      }
+    }
+  },
 })
